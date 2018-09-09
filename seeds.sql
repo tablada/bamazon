2a9b52fde165215a7c1e84f54a54985b8eb99fbd
Raul <raultablada@yahoo.com>
2018-09-09 18:51:15 -0400

    fix

--- seeds.sql
+++ seeds.sql
@@ -1,13 +1,13 @@
 USE bamazon;
 
 INSERT INTO products ( product_name,department_name, price, stock_quantity ) VALUES
-("Apple - iPhone X - White", "Phones", $999.99, 100),
-("Apple - iPhone X - Space Grey", "Phones", $999.99, 80),
-("Apple - MacBook - Gold", "Laptops", $1299.99, 50),
-("Apple - MacBook - Space Grey", "Laptops", $1299.99, 60),
-("Apple - MacBook - Silver", "Laptops", $1299.99, 40),
-("Apple - MacBook - Gold", "Laptops", $1299.99, 50),
-("Apple - MacBook Pro - Gold", "Laptops", $1799.99, 100),
-("Apple - MacBook Pro - Space Grey", "Laptops", $1799.99, 90),
-("Apple - MacBook Pro - Silver", "Laptops", $1799.99, 300),
-("Apple - iMac - Silver", "Computers", $2100.99, 10)
+("Apple - iPhone X - White", "Phones", 999.99, 100),
+("Apple - iPhone X - Space Grey", "Phones", 999.99, 80),
+("Apple - MacBook - Gold", "Laptops", 1299.99, 50),
+("Apple - MacBook - Space Grey", "Laptops", 1299.99, 60),
+("Apple - MacBook - Silver", "Laptops", 1299.99, 40),
+("Apple - MacBook - Gold", "Laptops", 1299.99, 50),
+("Apple - MacBook Pro - Gold", "Laptops", 1799.99, 100),
+("Apple - MacBook Pro - Space Grey", "Laptops", 1799.99, 90),
+("Apple - MacBook Pro - Silver", "Laptops", 1799.99, 300),
+("Apple - iMac - Silver", "Computers", 2199.99, 10)
