4f255f64791cd9f8944bda0546eb81d8bc27994e
Raul <raultablada@yahoo.com>
2018-09-09 18:55:54 -0400

    right save

--- customer.js
+++
@@ -1,8 +0,0 @@
-// MySQL Connection Parameters
-
-exports.mysql_params = {
-    host: process.env.MYSQL_SERVER,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD
-};
