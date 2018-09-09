c181fb99fac4c2149ada6f87dbfcca479e43c132
Raul <raultablada@yahoo.com>
2018-09-09 18:30:35 -0400

    schema

---
+++ schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT(10) NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(225),
+    department_name VARCHAR(50),
+    price DECIMAL (10,4),
+    PRIMARY KEY (item_id)
+);
+
+SELECT * FROM products;
